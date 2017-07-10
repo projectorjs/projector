95b4c878051741bab1a500a6f22a1bd911b8bb84
James Kyle <me@thejameskyle.com>
2017-07-10 15:10:32 +1000

    Fix types

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// @flow
 'use strict';
 
 const path = require('path');
@@ -5,7 +6,33 @@ const {spawn} = require('child_process');
 
 const CHILD_SCRIPT = path.join(__dirname, 'child.js');
 
-function projector(script /*: string */, exportName/*: string */, opts = {}) {
+/*::
+type Serializeable =
+  | null
+  | number
+  | boolean
+  | string
+  | Array<Serializeable>
+  | SerializeableObject;
+
+type SerializeableObject = {
+  [key: string]: Serializeable,
+};
+*/
+
+class StreamError extends Error {
+  /*::
+  code: number;
+  stderr: string;
+  */
+  constructor(message, code, stderr) {
+    super(message);
+    this.code = code;
+    this.stderr = stderr;
+  }
+}
+
+function projector(script /*: string */, exportName /*: string */, opts /*: SerializeableObject */ = {}) {
   return new Promise((resolve, reject) => {
     let child = spawn('node', [CHILD_SCRIPT, script, exportName]);
 
@@ -25,14 +52,11 @@ function projector(script /*: string */, exportName/*: string */, opts = {}) {
       if (code === 0) {
         resolve(stdout ? JSON.parse(stdout) : {});
       } else {
-        let err = new Error('Process errored with non-zero exit code.');
-        err.code = code;
-        err.stderr = stderr;
-        reject(err);
+        reject(new StreamError('Process errored with non-zero exit code.', code, stderr));
       }
     });
-
-    child.stdin.setEncoding('utf8');
+  
+    child.stdin.setDefaultEncoding('utf8');
     child.stdin.write(JSON.stringify(opts));
     child.stdin.end();
   });
